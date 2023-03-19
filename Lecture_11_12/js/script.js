94453fb145245556c9ff24b979746eba7b702108
Asif Rahman <digitalrahman@gmail.com>
2023-03-19 10:42:08 -0400

    update

---
+++ Lecture_11_12/js/script.js
@@ -0,0 +1,18 @@
+
+            function hello(text) {
+
+              var sayWelcome = text;
+
+              alert(sayWelcome);
+
+              console.log(sayWelcome);
+
+            }
+
+
+//list of events and handlers
+
+//https://eqsash.com/articles/vse-obrabotchiki-sobytiy-javascript-polnyy-spisok-s-opisaniem?l=en 
+
+
+
